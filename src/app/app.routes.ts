fb00bd56e2d569e4dd68a01d1196a38ec68ffd12
Evgeny <evgenymaillogin@gmail.com>
2025-09-29 18:11:55 +0400

    added default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,18 @@ export const routes: Routes = [
     path: 'documents',
     children: [
       {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '1'
+      },
+      {
         path: ':documentId',
         loadComponent: () => import('./document-viewer/document-viewer-layout').then(c => c.DocumentViewerLayout)
       }
     ]
+  },
+  {
+    path: '**',
+    redirectTo: 'documents/1'
   }
 ];
